635ffb94b785b90a390c4211a4b04df0f32ceafe
Abhinav Tanwar <tabhinav300@gmail.com>
2025-01-20 12:09:33 -0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Builder } from './pages/Builder';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/builder" element={<Builder />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
